87efdde3b3dd84633582b691c8cfec0d1864d30f
shayan <shayan4shayan@gmail.com>
2019-12-21 05:38:45 -0800

    find root with 4 different ways.

--- app/src/main/java/ir/shayan/addadi/Utils.kt
+++ app/src/main/java/ir/shayan/addadi/Utils.kt
@@ -4,6 +4,7 @@ import org.mariuszgromada.math.mxparser.Argument
 import org.mariuszgromada.math.mxparser.Expression
 
 fun Expression.eval(map: Pair<String, Double>) = apply {
+    removeAllArguments()
     addArguments(Argument(map.first, map.second))
 }.calculate()
 
@@ -11,28 +12,67 @@ fun Expression.findRoot(
     start: Double,
     end: Double,
     error: Double
-): Pair<Double, Double>? {
+): Double {
     // if value start point and value of end point has same sign then there is no way to find root
     // between these two points.
     var startPoint = start
     var endPoint = end
     var startVal = eval(Pair("x", start))
-    var endVal = eval(Pair("x", end))
+    val endVal = eval(Pair("x", end))
 
-    if (startVal * endVal > 0) return null
+    require(startVal * endVal <= 0) { "start and end points has same sign" }
 
     var lastRoot: Double
-    var currentRoot = 0.toDouble()
-
-    var lastVal: Double
-
+    var currentRoot = 0.0
     do {
         lastRoot = currentRoot
 
         val newPoint = (endPoint + startPoint) / 2
         val newVal = eval(Pair("x", newPoint))
+        println("$newPoint")
+        println("$newVal")
 
-        if (newVal == 0.toDouble()) return Pair(newPoint, newVal)
+        if (newVal == 0.toDouble()) return newPoint
+        if (newVal * startVal < 0) {
+            endPoint = newPoint
+
+        } else {
+            startPoint = newPoint
+            startVal = newVal
+        }
+
+        currentRoot = newPoint
+
+        println("error = " + (lastRoot - currentRoot))
+    } while (kotlin.math.abs(lastRoot - currentRoot) >= error)
+
+    return lastRoot
+}
+
+fun Expression.findRoot2(
+    start: Double,
+    end: Double,
+    error: Double
+): Double {
+
+    var startPoint = start
+    var endPoint = end
+    var startVal = eval(Pair("x", start))
+    var endVal = eval(Pair("x", end))
+
+    require(startVal * endVal <= 0) { "start and end points has same sign" }
+
+    var lastRoot: Double
+    var currentRoot = 0.0
+    do {
+        lastRoot = currentRoot
+
+        val newPoint = (startPoint * endVal - endPoint * startVal) / (endVal - startVal)
+        val newVal = eval(Pair("x", newPoint))
+        println("$newPoint")
+        println("$newVal")
+
+        if (newVal == 0.toDouble()) return newPoint
         if (newVal * startVal < 0) {
             endPoint = newPoint
             endVal = newVal
@@ -42,9 +82,55 @@ fun Expression.findRoot(
         }
 
         currentRoot = newPoint
-        lastVal = newVal
 
-    } while (lastRoot - currentRoot >= error)
+        println("error = " + (lastRoot - currentRoot))
+    } while (kotlin.math.abs(lastRoot - currentRoot) >= error)
 
-    return Pair(lastRoot, lastVal)
+    return lastRoot
+}
+
+fun Expression.findRoot3(
+    start: Double,
+    error: Double
+): Double {
+
+    val maxSteps = 100
+
+    var startPoint = start
+
+    val ne = Expression("$expressionString - x")
+
+    var lastRoot: Double
+    var currentRoot = startPoint
+    var step = 0
+    do {
+        step++
+        lastRoot = currentRoot
+
+        val newPoint = eval(Pair("x", currentRoot))
+        println("x= $newPoint")
+        currentRoot = newPoint
+
+        println("error = " + (lastRoot - currentRoot))
+    } while (kotlin.math.abs(lastRoot - currentRoot) >= error && step < maxSteps)
+
+    return currentRoot
+}
+
+fun Expression.findRoot4(start: Double, moshtag: Expression, error: Double): Double {
+    val maxStep = 100
+
+    var lastRoot: Double
+    var currentRoot = start
+    var step = 0
+
+    do {
+        step++
+        lastRoot = currentRoot
+        val newPoint =
+            currentRoot - (eval(Pair("x", currentRoot)) / moshtag.eval(Pair("x", currentRoot)))
+        currentRoot = newPoint
+    } while (lastRoot - currentRoot >= error && step < maxStep)
+
+    return currentRoot
 }
--- app/src/test/java/ir/shayan/addadi/ExampleUnitTest.kt
+++ app/src/test/java/ir/shayan/addadi/ExampleUnitTest.kt
@@ -13,26 +13,36 @@ import org.mariuszgromada.math.mxparser.Expression
  */
 class ExampleUnitTest {
 
-    var function: Expression? = null
+    lateinit var function: Expression
 
     @Before
     fun setup() {
-        function = Expression("x*2 + 5*x^2 -15")
+        function = Expression("x^2 - 2")
+    }
+
+    @Test
+    fun no_argument() {
+        assertEquals(15.toDouble(), function.calculate(), 0.01)
     }
 
     @Test
     fun one_argument() {
-        assertEquals((-8).toDouble(),function?.eval(Pair("x",1.toDouble())))
-        assertEquals((-15).toDouble(),function?.eval(Pair("x",0.toDouble())))
+        assertEquals((-8).toDouble(), function.eval(Pair("x", 1.toDouble())), 0.01)
+        assertEquals((-15).toDouble(), function.eval(Pair("x", 0.toDouble())), 0.01)
     }
 
     @Test
-    fun two_arguments() {
-
+    fun root() {
+        assertEquals(1.5, function.findRoot(1.0, 2.0, 0.01), 0.05)
     }
 
     @Test
-    fun three_arguments(){
+    fun root2() {
+        assertEquals(1.5, function.findRoot2(1.0, 2.0, 0.01), 0.05)
+    }
 
+    @Test
+    fun root3() {
+        assertEquals(-1.0, function.findRoot3(1.5, 0.01), 0.05)
     }
 }
